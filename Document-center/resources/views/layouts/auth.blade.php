b182111e35d4e59fffaf83f17e04d80065311ca3
putrinurifa <putrinurifa818@gmail.com>
2022-07-05 09:50:04 +0700

    logo

--- Document-center/resources/views/layouts/auth.blade.php
+++ Document-center/resources/views/layouts/auth.blade.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Login Register | Notika - Notika Admin Template</title>
+    <title>Login Admin</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
 		============================================ -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets') }}/img/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets') }}/img/ship.png">
     <!-- Google Fonts
 		============================================ -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
