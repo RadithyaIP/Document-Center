81a68db5af866b3ea4d77403e4d3f46ccc61e969
putrinurifa <putrinurifa818@gmail.com>
2022-08-01 16:13:03 +0700

    view

--- Document-center/resources/views/Categories/View.blade.php
+++ Document-center/resources/views/Categories/View.blade.php
@@ -2,150 +2,185 @@
 
 @section('content')
 <div class="row">
-    <div class="col-xl-12">
+    <div class="col-lg-12">
         <div class="card">
-            <div class="card-header align-items-center d-flex">
-                <h4 class="card-title mb-0 flex-grow-1">{{ $kategoris->nama }}</h4>
-
-                <form class="float-right form-inline" id="searchForm" method="get" action="{{ route('home') }}" role="search">
-                    <div class="form-group">
-                        <input type="text" name="keyword" class="form-control" id="Keyword" aria-describedby="Keyword" placeholder="Keyword" value="{{request()->query('keyword')}}">
-                    </div>
-                    <button type="submit" class="btn btn-primary mx-2">Cari</button>
-                    <a href="{{ route('home') }}">
-                        <button type="button" class="btn btn-danger">Reset</button>
-                    </a>
-                </form>
-                <!-- <div class="flex-shrink-0">
-                    <div class="dropdown card-header-dropdown">
-                        <a class="text-reset dropdown-btn" href="#" data-bs-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            <span class="fw-semibold text-uppercase fs-12">Sort by:
-                            </span><span class="text-muted">Today<i class="mdi mdi-chevron-down ms-1"></i></span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end">
-                            <a class="dropdown-item" href="#">Today</a>
-                            <a class="dropdown-item" href="#">Yesterday</a>
-                            <a class="dropdown-item" href="#">Last 7 Days</a>
-                            <a class="dropdown-item" href="#">Last 30 Days</a>
-                            <a class="dropdown-item" href="#">This Month</a>
-                            <a class="dropdown-item" href="#">Last Month</a>
-                        </div>
-                    </div>
-                </div> -->
+            <div class="card-header">
+                <h4 class="card-title mb-0">{{ $kategoris->nama }}</h4>
             </div><!-- end card header -->
 
             <div class="card-body">
-                <div class="table-responsive table-card">
-                    <table class="table table-hover table-centered align-middle table-nowrap mb-0">
-                        <tbody>
-                            @foreach($dokumens as $dokumen)
-                            @if($users->divisi_id == $dokumen->divisi
-                            && $dokumen->kategori_id == $kategoris->id
-                            && $dokumen->is_active == '1')
-                            <tr>
-                                <td>
-                                    <div class="d-flex align-items-center">
-                                        <div>
-                                            <h5 class="fs-14 my-1"><a href="{{ route('Dokumen.show', $dokumen->id) }}"
-                                                    class="text-reset">{{"$dokumen->nama_dokumen"}}</a></h5>
-                                            <!-- <span class="text-muted"></span> -->
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <span class="text-muted">Nomor Dokumen</span>
-                                    <h5 class="fs-14 my-1 fw-normal">{{"$dokumen->no_dokumen"}}</h5>
-                                </td>
+                <div id="customerList">
+                    <div class="row g-4 mb-3">
+                        <div class="col-sm-auto">
+                            <div>
+                                <button type="button" class="btn btn-success add-btn" data-bs-toggle="modal"
+                                    id="create-btn" data-bs-target="#showModal"><i
+                                        class="ri-add-line align-bottom me-1"></i> Add</button>
+                                <button class="btn btn-soft-danger" onClick="deleteMultiple()"><i
+                                        class="ri-delete-bin-2-line"></i></button>
+                            </div>
+                        </div>
+                        <div class="col-sm">
+                            <div class="d-flex justify-content-sm-end">
+                                <div class="search-box ms-2">
+                                    <input type="text" class="form-control search" placeholder="Search...">
+                                    <i class="ri-search-line search-icon"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                                <td>
-                                    <a href="{{ route('downloadFile', $dokumen->file_name) }}">
-                                        <button type="submit" class="btn btn-primary">
-                                            Download
-                                        </button>
-                                    </a>
-                                </td>
-                                <td>
-                                    <a href="{{ route('viewFile', $dokumen->file_name) }}">
-                                        <button type="submit" class="btn btn-primary">
-                                            View
-                                        </button>
-                                    </a>
-                                </td>
-                                <td>
-                                    <a href="{{ route('Dokumen.show', $dokumen->id) }}">
-                                        <button type="submit" class="btn btn-primary">
-                                            Details
-                                        </button>
-                                    </a>
-                                </td>
-                            </tr>
+                    <div class="table-responsive table-card mt-3 mb-1">
+                        <table class="table align-middle table-nowrap" id="customerTable">
+
+                            <thead class="table-light">
+                                <tr>
+                                    <th scope="col" style="width: 50px;">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" id="checkAll"
+                                                value="option">
+                                        </div>
+                                    </th>
+                                    <th class="sort" data-sort="customer_name">User</th>
+                                    <th class="sort" data-sort="no_dokumen">No Dokumen</th>
+                                    <th class="sort" data-sort="nama_dokumen">Nama Dokumen</th>
+                                    <th class="sort" data-sort="revisi">Revisi Dokumen</th>
+                                    <th class="sort" data-sort="status">Delivery Status</th>
+                                    <th class="sort" data-sort="action">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody class="list form-check-all">
+                                <?php
+                                $i = 0;
+                                ?>
+                                @foreach($dokumens as $dokumen)
+                                @if($users->divisi_id == $dokumen->divisi
+                                && $dokumen->kategori_id == $kategoris->id
+                                && $dokumen->is_active == '1')
+                                <?php
+                                    $i++;
+                            ?>
+                                <tr>
+                                    <th scope="row">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="chk_child"
+                                                value="option1">
+                                        </div>
+                                    </th>
+                                    <td class="id" style="display:none;"><a href="javascript:void(0);"
+                                            class="fw-medium link-primary">#VZ2101</a></td>
+                                    <td class="customer_name">{{ "$users->name" }}</td>
+                                    <td class="no_dokumen">{{"$dokumen->no_dokumen"}}</td>
+                                    <td class="nama_dokumen">{{"$dokumen->nama_dokumen"}}</td>
+                                    <td class="revisi">{{"$dokumen->created_at"}}</td>
+                                    <td class="status"><span
+                                            class="badge badge-soft-success text-uppercase">Active</span></td>
+                                    <td>
+                                        <div class="d-flex gap-2">
+                                            <div class="edit">
+                                                <button class="btn btn-sm btn-success edit-item-btn"
+                                                    data-bs-toggle="modal" data-bs-target="#showModal">Edit</button>
+                                            </div>
+                                            <div class="remove">
+                                                <button class="btn btn-sm btn-danger remove-item-btn"
+                                                    data-bs-toggle="modal"
+                                                    data-bs-target="#deleteRecordModal">Remove</button>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
                             @endif
                             @endforeach
-                            <!-- <tr>
-                                <td>
-                                    <div class="d-flex align-items-center">
-                                        <div class="avatar-sm bg-light rounded p-1 me-2">
-                                            <img src="assets/images/products/img-1.png" alt=""
-                                                class="img-fluid d-block" />
-                                        </div>
-                                        <div>
-                                            <h5 class="fs-14 my-1"><a href="apps-ecommerce-product-details.html"
-                                                    class="text-reset">Branded T-Shirts</a></h5>
-                                            <span class="text-muted">24 Apr 2021</span>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <h5 class="fs-14 my-1 fw-normal">$29.00</h5>
-                                    <span class="text-muted">Price</span>
-                                </td>
-                                <td>
-                                    <h5 class="fs-14 my-1 fw-normal">62</h5>
-                                    <span class="text-muted">Orders</span>
-                                </td>
-                                <td>
-                                    <h5 class="fs-14 my-1 fw-normal">510</h5>
-                                    <span class="text-muted">Stock</span>
-                                </td>
-                                <td>
-                                    <h5 class="fs-14 my-1 fw-normal">$1,798</h5>
-                                    <span class="text-muted">Amount</span>
-                                </td>
-                            </tr> -->
-                        </tbody>
-                    </table>
-                </div>
-
-                <div class="align-items-center mt-4 pt-2 justify-content-between d-flex">
-                    <div class="flex-shrink-0">
-
-                        <div class="d-flex">
-                            {{ $dokumens->links('pagination::bootstrap-4') }}
+                            </tbody>
+                        </table>
+                        <div class="noresult" style="display: none">
+                            <div class="text-center">
+                                <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
+                                    colors="primary:#121331,secondary:#08a88a" style="width:75px;height:75px">
+                                </lord-icon>
+                                <h5 class="mt-2">Sorry! No Result Found</h5>
+                                <p class="text-muted mb-0">We've searched more than 150+ Orders We did not find any
+                                    orders for you search.</p>
+                            </div>
                         </div>
-                    <!-- </div>
-                    <ul class="pagination pagination-separated pagination-sm mb-0">
-                        <li class="page-item disabled">
-                            <a href="#" class="page-link">←</a>
-                        </li>
-                        <li class="page-item">
-                            <a href="#" class="page-link">1</a>
-                        </li>
-                        <li class="page-item active">
-                            <a href="#" class="page-link">2</a>
-                        </li>
-                        <li class="page-item">
-                            <a href="#" class="page-link">3</a>
-                        </li>
-                        <li class="page-item">
-                            <a href="#" class="page-link">→</a>
-                        </li>
-                    </ul>
-                </div> -->
+                    </div>
 
+                    <div class="d-flex justify-content-end">
+                        <div class="pagination-wrap hstack gap-2">
+                            <a class="page-item pagination-prev disabled" href="#">
+                                Previous
+                            </a>
+                            <ul class="pagination listjs-pagination mb-0"></ul>
+                            <a class="page-item pagination-next" href="#">
+                                Next
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end card -->
+        </div>
+        <!-- end col -->
+    </div>
+    <!-- end col -->
+</div>
+
+<div class="modal fade" id="showModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header bg-light p-3">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                    id="close-modal"></button>
             </div>
+            <form>
+                <div class="modal-body">
+
+                    <div class="mb-3" id="modal-id" style="display: none;">
+                        <label for="id-field" class="form-label">ID</label>
+                        <input type="text" id="id-field" class="form-control" placeholder="ID" readonly />
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="customername-field" class="form-label">No Dokumen</label>
+                        <input type="text" id="customername-field" class="form-control" placeholder="Enter Name"
+                            required />
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="email-field" class="form-label">Nama Dokumen</label>
+                        <input type="email" id="email-field" class="form-control" placeholder="Enter Email" required />
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="phone-field" class="form-label">Revisi Dokumen</label>
+                        <input type="text" id="phone-field" class="form-control" placeholder="Enter Phone no."
+                            required />
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="date-field" class="form-label">Joining Date</label>
+                        <input type="text" id="date-field" class="form-control" placeholder="Select Date" required />
+                    </div>
+
+                    <div>
+                        <label for="status-field" class="form-label">Status</label>
+                        <select class="form-control" data-trigger name="status-field" id="status-field">
+                            <option value="">Status</option>
+                            <option value="Active">Active</option>
+                            <option value="Block">Block</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <div class="hstack gap-2 justify-content-end">
+                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-success" id="add-btn">Add Customer</button>
+                        <button type="button" class="btn btn-success" id="edit-btn">Update</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-</div> <!-- end row-->
+</div>
 
 @endsection
